11cb8b6c4dd7bfce6fea796592236e81a79d1363
Keisuke Kawata <keisukekawata@iss.u-tokyo.ac.jp>
2025-10-01 15:16:41 +0900

    update 03.r

--- Public/03prediction.r
+++ Public/03prediction.r
@@ -1,24 +1,32 @@
+set.seed(1)
+
 library(tidyverse)
 
-data <- read_csv("data.csv")
+data = read_csv("data.csv")
 
-train <- sample(1:5474, 4000) # ctr + Enter
+train = sample(1:5474, 4000) # ctr + Enter
 
-data_train <- data[train, ]
-data_test <- data[-train, ]
+data_train = data[train,]
+data_test = data[-train,]
 
-model1 <- lm(Price ~ Size, data_train)
+model1 = lm(Price ~ Size, data_train)
 
-model2 <- lm(Price ~ Size + Tenure + Distance + District, data_train)
+model2 = lm(Price ~ Size + Tenure + Distance + District, data_train)
 
-pred1 <- predict(model1, data_test)
+model3 = hdm::rlasso(Price ~ ., data_train, post = FALSE) # LASSO
 
-pred2 <- predict(model2, data_test)
+pred1 = predict(model1, data_test)
 
-eval1 <- mean((data_test$Price - pred1)^2)
+pred2 = predict(model2, data_test)
 
-eval2 <- mean((data_test$Price - pred2)^2)
+pred3 = predict(model3, data_test) # LASSO
 
-(eval1 - eval2) / eval1
+eval1 = mean((data_test$Price - pred1)^2)
+
+eval2 = mean((data_test$Price - pred2)^2)
+
+eval3 = mean((data_test$Price - pred3)^2) # LASSO
+
+(eval1 - eval2)/eval1
 
 # ctr + A -> ctr + Enter
